7ecfabaeab75369c7d11d72f229ea15d18c33b32
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-02-04 14:44:54 -0200

    Implementando o protótipo da exibição de venda de produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { MenuItem } from 'primeng/components/common/menuitem';
+import { Message } from 'primeng/components/common/message';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+
+  items: MenuItem[];
+
+  msgs: Message[] = [];
+
+  // tslint:disable-next-line:no-inferrable-types
+  activeIndex: number = 1;
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+      this.items = [{
+              label: 'Produtos disponíveis',
+              command: (event: any) => {
+                  this.activeIndex = 0;
+                  this.msgs.length = 0;
+                  this.msgs.push({severity: 'info', summary: 'First Step', detail: event.item.label});
+              }
+          },
+          {
+              label: 'Carrinho',
+              command: (event: any) => {
+                  this.activeIndex = 1;
+                  this.msgs.length = 0;
+                  this.msgs.push({severity: 'info', summary: 'Seat Selection', detail: event.item.label});
+              }
+          },
+          {
+              label: 'Confirmar compra',
+              command: (event: any) => {
+                  this.activeIndex = 2;
+                  this.msgs.length = 0;
+                  this.msgs.push({severity: 'info', summary: 'Pay with CC', detail: event.item.label});
+              }
+          }
+      ];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { StepsModule } from 'primeng/steps';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StepsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
