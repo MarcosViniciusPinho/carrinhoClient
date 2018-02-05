bb8478efc9f9ae64f5d0506fc0dd4dee65648b98
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-02-04 23:59:54 -0200

    Implementando o protótipo da tela de produtos para venda.

--- src/app/components/cards/cards.module.ts
+++ src/app/components/cards/cards.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
 
 import { CardsComponent } from './cards.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CardModule
+    CardModule,
+    InputTextModule
   ],
   declarations: [CardsComponent],
   exports: [CardsComponent]
