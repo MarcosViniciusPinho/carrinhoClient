4353ef06b358903b14fd0904b75e0568f2d7addd
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-02-08 21:17:50 -0200

    Implementado a retirada do item da lista de produtos ao adicinar no carrinho.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -11,40 +11,27 @@ export class CardsComponent implements OnInit {
   produtosEscolhidos = [];
 
   ngOnInit() {
-    this.produtos = [{nome: 'Sabão', preco: '20.00', id: 1},
-                    {nome: 'Geladeira', preco: '5.000', id: 5},
-                    {nome: 'Televisão', preco: '8000.00', id: 10},
-                    {nome: 'PS4', preco: '4000.00', id: 12},
-                    {nome: 'Ar Condicionado', preco: '900.00', id: 15},
-                    {nome: 'Celular Smartphone', preco: '2000.00', id: 16},
-                    {nome: 'Sabão', preco: '20.00', id: 20},
-                    {nome: 'Geladeira', preco: '5.000', id: 22},
-                    {nome: 'Televisão', preco: '8000.00', id: 24},
-                    {nome: 'PS4', preco: '4000.00', id: 25},
-                    {nome: 'Ar Condicionado', preco: '900.00', id: 31},
-                    {nome: 'Celular Smartphone', preco: '2000.00', id: 33},
-                    {nome: 'Sabão', preco: '20.00', id: 36},
-                    {nome: 'Geladeira', preco: '5.000', id: 40},
-                    {nome: 'Televisão', preco: '8000.00', id: 42},
-                    {nome: 'PS4', preco: '4000.00', id: 45},
-                    {nome: 'Ar Condicionado', preco: '900.00', id: 47},
-                    {nome: 'Celular Smartphone', preco: '2000.00', id: 49},
-                    {nome: 'Sabão', preco: '20.00', id: 52},
-                    {nome: 'Geladeira', preco: '5.000', id: 55},
-                    {nome: 'Televisão', preco: '8000.00', id: 60},
-                    {nome: 'PS4', preco: '4000.00', id: 63},
-                    {nome: 'Ar Condicionado', preco: '900.00', id: 64},
-                    {nome: 'Celular Smartphone', preco: '2000.00', id: 67},
-                    {nome: 'Sabão', preco: '20.00', id: 75},
-                    {nome: 'Geladeira', preco: '5.000', id: 80},
-                    {nome: 'Televisão', preco: '8000.00', id: 82},
-                    {nome: 'PS4', preco: '4000.00', id: 85},
-                    {nome: 'Ar Condicionado', preco: '900.00', id: 90},
-                    {nome: 'Celular Smartphone', preco: '2000.00', id: 95}];
+    this.produtos = [{nome: 'Sabão', preco: Math.floor((Math.random() * 70.5)), id: 1},
+                    {nome: 'Geladeira', preco: Math.floor((Math.random() * 70.5)), id: 5},
+                    {nome: 'Televisão', preco: Math.floor((Math.random() * 70.5)), id: 10},
+                    {nome: 'PS4', preco: Math.floor((Math.random() * 70.5)), id: 12},
+                    {nome: 'Ar Condicionado', preco: Math.floor((Math.random() * 70.5)), id: 15},
+                    {nome: 'Celular Smartphone', preco: Math.floor((Math.random() * 70.5)), id: 16},
+                    {nome: 'Computador Ultima Geração', preco: Math.floor((Math.random() * 70.5)), id: 20},
+                    {nome: 'Maquina de fotografia', preco: Math.floor((Math.random() * 70.5)), id: 22},
+                    {nome: 'Livros', preco: Math.floor((Math.random() * 70.5)), id: 24},
+                    {nome: 'Maquina de lavar', preco: Math.floor((Math.random() * 70.5)), id: 25},
+                    {nome: 'Impressora', preco: Math.floor((Math.random() * 70.5)), id: 31},
+                    {nome: 'Mouse', preco: Math.floor((Math.random() * 70.5)), id: 33},
+                    {nome: 'Mesa para PC', preco: Math.floor((Math.random() * 70.5)), id: 36},
+                    {nome: 'Cadeira para PC', preco: Math.floor((Math.random() * 70.5)), id: 40},
+                    {nome: 'Mini ventilador', preco: Math.floor((Math.random() * 70.5)), id: 42},
+                    {nome: 'Ventilador', preco: Math.floor((Math.random() * 70.5)), id: 45},
+                    {nome: 'Mouse Pad', preco: Math.floor((Math.random() * 70.5)), id: 47}];
   }
 
-  selecionarProduto(produto, row) {
-    this.produtos.splice(row, 1);
+  selecionarProduto(produto) {
+    this.produtos = this.produtos.filter(obj => obj !== produto);
     this.produtosEscolhidos.push(produto);
   }
 
