52e436ee6960a9c218d227dcaf902d6e33e3aa85
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-02-10 19:56:11 -0200

    Melhorando o layout da primeira e segunda etapa.

--- src/app/components/carrinho-produtos/carrinho-produtos.component.ts
+++ src/app/components/carrinho-produtos/carrinho-produtos.component.ts
@@ -11,11 +11,15 @@ export class CarrinhoProdutosComponent implements OnInit {
   ngOnInit() {
 
     this.produtosEscolhidos = [{nome: 'Sabão', preco: Math.floor((Math.random() * 70.5)), id: 1, quantidade: 1},
-    {nome: 'Geladeira', preco: Math.floor((Math.random() * 70.5)), id: 5, quantidade: 1},
-    {nome: 'Televisão', preco: Math.floor((Math.random() * 70.5)), id: 10, quantidade: 1},
-    {nome: 'PS4', preco: Math.floor((Math.random() * 70.5)), id: 12, quantidade: 1},
-    {nome: 'Ar Condicionado', preco: Math.floor((Math.random() * 70.5)), id: 15, quantidade: 1}];
+                              {nome: 'Geladeira', preco: Math.floor((Math.random() * 70.5)), id: 5, quantidade: 1},
+                              {nome: 'Televisão', preco: Math.floor((Math.random() * 70.5)), id: 10, quantidade: 1},
+                              {nome: 'PS4', preco: Math.floor((Math.random() * 70.5)), id: 12, quantidade: 1},
+                              {nome: 'Ar Condicionado', preco: Math.floor((Math.random() * 70.5)), id: 15, quantidade: 1}];
 
+  }
+
+  excluirProduto(produto) {
+    this.produtosEscolhidos = this.produtosEscolhidos.filter(obj => obj !== produto);
   }
 
 }
