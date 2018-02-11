141c7ce1d6b67fd2c1717c74fbc20a7369bf13b0
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-02-11 21:06:16 -0200

    Implementado o calculo total a pagar do usuário na etapa 2.

--- src/app/components/carrinho-produtos/carrinho-produtos.component.ts
+++ src/app/components/carrinho-produtos/carrinho-produtos.component.ts
@@ -8,18 +8,38 @@ export class CarrinhoProdutosComponent implements OnInit {
 
   produtosEscolhidos = [];
 
+  // tslint:disable-next-line:no-inferrable-types
+  totalAPagar: number = 0;
+
   ngOnInit() {
 
-    this.produtosEscolhidos = [{nome: 'Sabão', preco: Math.floor((Math.random() * 70.5)), id: 1, quantidade: 1},
-                              {nome: 'Geladeira', preco: Math.floor((Math.random() * 70.5)), id: 5, quantidade: 1},
-                              {nome: 'Televisão', preco: Math.floor((Math.random() * 70.5)), id: 10, quantidade: 1},
-                              {nome: 'PS4', preco: Math.floor((Math.random() * 70.5)), id: 12, quantidade: 1},
-                              {nome: 'Ar Condicionado', preco: Math.floor((Math.random() * 70.5)), id: 15, quantidade: 1}];
+    this.produtosEscolhidos = [{nome: 'Sabão', preco: 50, id: 1, quantidade: 1},
+                              {nome: 'Geladeira', preco: 50, id: 5, quantidade: 1},
+                              {nome: 'Televisão', preco: 50, id: 10, quantidade: 1},
+                              {nome: 'PS4', preco: 50, id: 12, quantidade: 1},
+                              {nome: 'Ar Condicionado', preco: 50, id: 15, quantidade: 1}];
+    this.calcularTotalAoIniciar();
 
+  }
+
+  calcularTotalAoIniciar() {
+    for (const produto of this.produtosEscolhidos) {
+        this.totalAPagar += produto.preco * produto.quantidade;
+    }
+  }
+
+  calcularTotalAposQuantidadeDeUmProdutoSerAlterada() {
+    this.totalAPagar = 0;
+    this.calcularTotalAoIniciar();
+  }
+
+  calcularTotalAposExclusaoDeUmProduto(produto) {
+    this.totalAPagar -= produto.preco * produto.quantidade;
   }
 
   excluirProduto(produto) {
     this.produtosEscolhidos = this.produtosEscolhidos.filter(obj => obj !== produto);
+    this.calcularTotalAposExclusaoDeUmProduto(produto);
   }
 
 }
