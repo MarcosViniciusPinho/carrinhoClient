8b37d2ff8cc978a854bf8e5b3f1936b9815dad40
Marcos Pinho <marcosjava2008@gmail.com>
2018-08-19 23:55:07 -0300

    Correção de BUG ao chamar serviço

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlCarrinhoApi: 'https://18.208.161.95:8080',
+  urlCarrinhoApi: 'http://18.208.161.95:8080',
   urlLocalidadeApi: 'http://52.70.155.42:8080'
 };
